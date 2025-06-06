da131232f20edd02e82e76d3a3f0975cbf702a69
Natsume1710 <162396185+Natsume1710@users.noreply.github.com>
2025-06-06 17:30:34 +0800

    Create news_classification.py

---
+++ Project/NLP/news_classification.py
@@ -0,0 +1,5 @@
+def news_classification():
+    print("\u2705 使用 THUCNews 语料 + BERT 微调进行分类")
+    print("1. 数据集链接：https://thunlp.oss-cn-qingdao.aliyuncs.com/THUCNews.zip")
+    print("2. 示例模型：IDEA-CCNL/Erlangshen-Roberta-330M-Chinese")
+    print("3. 微调建议使用 transformers Trainer + datasets.load_dataset")
